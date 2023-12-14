9559d67b9bd82649b81cf6a359efea9ea8547f0f
GUNTUKU NARESH <guntuku001@gmail.com>
2023-12-14 14:05:07 +0530

    Done

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer'
+import qr from 'qr-image'
+import fs from 'fs'
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {message:"Type URL.",
+    name:"URL"},
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url=answers.URL
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    //console.log(answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
